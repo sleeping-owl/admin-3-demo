4b6331197959468341440324133163a094e69483
Sleeping Owl <owl.sleeping@yahoo.com>
2015-04-30 22:30:19 +0400

    Template link added

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,24 @@
 <div class="row">
 	<div class="col-md-6 text-center">
-		<a class="btn btn-primary" href="{{ route('admin.change-template', 0) }}">
-			Switch template to sb-admin-2
-		</a>
+		<div class="row">
+			<a class="btn btn-primary" href="{{ route('admin.change-template', 0) }}">
+				Switch template to sb-admin-2
+			</a>
+		</div>
+		<div class="row">
+			<br/>
+			This template is built-in.
+		</div>
 	</div>
 	<div class="col-md-6 text-center">
-		<a class="btn btn-primary" href="{{ route('admin.change-template', 1) }}">
-			Switch template to adminLTE
-		</a>
+		<div class="row">
+			<a class="btn btn-primary" href="{{ route('admin.change-template', 1) }}">
+				Switch template to adminLTE
+			</a>
+		</div>
+		<div class="row">
+			<br/>
+			To use this template in your projects see <a href="https://github.com/sleeping-owl/admin-lte-template">admin-lte-template</a> package.
+		</div>
 	</div>
 </div>
